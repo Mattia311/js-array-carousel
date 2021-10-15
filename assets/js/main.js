78f4b8b8fbf096a7503319c8f2734db720de8694
Mattia311 <mattia.space98@gmail.com>
2021-10-15 18:24:26 +0200

    parte strutturale complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,26 +25,35 @@ const text = [
 
 const arrowTopButtonElement = document.getElementById('arrow-top');
 const arrowBottomButtonElement = document.getElementById('arrow-bottom');
+const img = document.querySelector('#slider')
+var titolo = document.getElementById('text').innerHTML;
+var corpo = document.getElementById('text1').innerHTML;
 
 // Al click scorre le immagini
-
+var corrente = 0
+var n_img = items.length -1
+var titolo = title.length -1
+var testo = text.length -1
 arrowBottomButtonElement.addEventListener('click', function (){
-    for (let i = 0; i < items.length - 1; i++) {
-        const element = items[i]; 
-       
-
-            document.getElementById('slider').src = items[1]
-            console.log(items);
-        
-       
-            document.getElementById('slider').src = items[2]
-
-        
-        
-        
-       
-
-        
+    corrente++;
+    if((corrente > n_img) && (corrente > titolo) && (corrente > testo)){
+        corrente = 0;
     }
+    console.log(corrente);
+    img.setAttribute('src','./' + items[corrente])
+    document.querySelector('text').innerHTML += titolo
+    document.querySelector('text1').innerHTML += testo
     
+    
+   /* console.log(items[corrente]);
+    console.log(title[corrente]);
+    console.log(text[corrente]);*/
+});
+arrowTopButtonElement.addEventListener('click', function (){
+    corrente--;
+    if(corrente == -1){
+        corrente = n_img;
+    }
+    console.log(corrente);
+    img.setAttribute('src','./' + items[corrente])
 });
