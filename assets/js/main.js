151033a9bf6aeaf0997f9c7e51cad34a22086c4d
Mattia311 <mattia.space98@gmail.com>
2021-10-15 15:04:45 +0200

    ciclo immagini

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,30 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
+// diamo ai bottoni la possibilità di compiere un azione per far scorrere le immagini
+
+const arrowTopButtonElement = document.getElementById('arrow-top');
+const arrowBottomButtonElement = document.getElementById('arrow-bottom');
+
+// Al click scorre le immagini
+
+arrowBottomButtonElement.addEventListener('click', function (){
+    for (let i = 0; i < items.length - 1; i++) {
+        const element = items[i]; 
+       
+
+            document.getElementById('slider').src = items[1]
+            console.log(items);
+        
+       
+            document.getElementById('slider').src = items[2]
+
+        
+        
+        
+       
+
+        
+    }
+    
+});
